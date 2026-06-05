27dea3b2296e7a3a5dabbb205f8fa0ef210b2688
NyleLuza <Jluza21@gmail.com>
2026-06-04 21:35:05 -0700

    converting from pdf to png for tesseract processing

--- model.py
+++ model.py
@@ -1,1 +1,32 @@
-print("hello world")
+import pytesseract
+import pymupdf
+from pathlib import Path
+from PIL import Image
+import io
+
+path = Path("pdf")
+
+def convert_pdf_to_png(pdf_path, output_folder, i):
+    # opens pdf doc using pymudf library
+    doc = pymupdf.open(pdf_path)
+    # iterates through each page in the pdf
+    for page_num in range(len(doc)):
+        page = doc[page_num]
+        # initializes zoom to clear up image for clarity
+        mat = pymupdf.Matrix(2,2)
+        
+        # basically appliess a transformation onto a new canvas with the zoom
+        pix = page.get_pixmap(matrix=mat)
+        pix.save(f"{output_folder}/receipt_{i}.png")
+        print("complete 3")
+
+i = 1
+# iterate through each item in directory
+for file_path in path.iterdir():
+    print("complete 1")
+    # checks if each item in the directory is not a subdirectory
+    if file_path.is_file():
+        print("complete 2")
+        convert_pdf_to_png(file_path, "images", i)
+        i+=1
+        
